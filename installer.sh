e83b0d87579fe114547abe06c391aab9b7db1990
Ricardo <ricardoromeroesq@gmail.com>
2026-07-30 03:53:01 +0000

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+sudo pacman -Syu
+
+cd ~/
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~/ricardos-dots-arch
+
+yay
+yay -Syu sway swaylock wofi firefox nautilus git foot fish ufetch-git ttf-dejavu-nerd swayidle wmenu grim
+
+mkdir -p ~/.config/sway
+cp -r config ~/.config/sway/
+cp -r scripts ~/.config/sway/
+chmod +x ~/.config/sway/scripts/*
+mkdir -p ~/Pictures/Wallpapers/
+cp arch.jpg ~/Pictures/Wallpapers/
+cp -r fish ~/.config/
+cp -r foot ~/.config/
+
+echo "==========================================================="
+echo "                  Installation complete!                   "
+echo "  You can launch your desktop environment by typing: sway  "
+echo "==========================================================="
+echo "You can also use the extra wallpapers by copying them into the newly created ~/Pictures/Wallpapers."
+echo "Also, please load the drivers if you haven't by:
+echo "eg, for Intel, kldload i915kms for AMD, kldload amdgpu." 

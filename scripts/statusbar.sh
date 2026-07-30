35bc6d369c3e4981c3b0bb1b37d1000587fcc912
Ricardo <ricardoromeroesq@gmail.com>
2026-07-30 04:09:02 +0000

    Update statusbar.sh

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -2,7 +2,7 @@
 while true; do
     DATE=$(date "+%a %b %d")
     TIME=$(date "+%I:%M %p")
-    BATT=$(acpiconf -i 0 | awk -F: '/Remaining capacity/ {gsub(/%/,"",$2); print $2}' | tr -d ' ')
+    BATT=$(acpi -b)
 
     echo "-/ $DATE / $TIME /- ${BATT}% Charged    "
     sleep 30

8a7363a4e7413b8b41619ed38f0cf61bbd7d9d0b
Ricardo <ricardoromeroesq@gmail.com>
2026-07-30 01:14:25 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     function fish_greeting
-        set_color red
+        set_color blue
         ufetch
 	echo ""
-echo "THE POWER TO SERVE"
+echo "Keep it simple, stupid."
 echo ""
         set_color normal
     end
